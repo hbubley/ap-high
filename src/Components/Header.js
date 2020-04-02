520af1d6db41b1b8654c8471bcb46426df9fface
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-04-02 12:42:26 -0500

    Added monkey

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,18 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 export default function Header() {
+    const smokeImages = [
+        'https://i.imgur.com/EtMcQ33.png',
+        'https://i.imgur.com/6Z1clIX.png',
+        'https://i.imgur.com/YmcB0HL.png',
+        'https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png',
+      ]
+   
+    
     return (
         <div>
             <nav>
-                <Link to="/"><i className="fal fa-monkey"></i></Link>
+                <Link to="/"><i className="fal fa-monkey icon"></i></Link>
             </nav>
         </div>
     )
